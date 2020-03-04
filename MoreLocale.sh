090de94c7a4276217a68b75cd1b37ad2a319fe94
iKeramat <iKeramat80@gmail.com>
2020-03-04 18:18:52 +0330

    upload files

---
+++ MoreLocale.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo " "
+echo "***************************************************"
+echo "            Morelocal enabler script               "
+echo "***************************************************"
+echo " "
+adb kill-server
+adb install morelocale.apk
+adb shell "pm grant jp.co.c_lis.ccl.morelocale android.permission.CHANGE_CONFIGURATION"
+adb wait-for-device
+adb shell "pm grant jp.co.c_lis.ccl.morelocale android.permission.CHANGE_CONFIGURATION"
+echo " "
+echo "***************************************************"
+echo "                      Done.                        "
+echo "***************************************************"
+echo " "
+echo " Enjoy your phone! "
---
+++ README.md
@@ -0,0 +1,12 @@
+## Required package
+You need to install adb follow this [guide](https://github.com/iKeramat/adb-fastboot-linux#installation) for installation 
+## How to use?
+Download as zip or clone this repo.\
+```bash
+chmod +x MoreLocal.sh
+```
+```bash
+./MoreLocal.sh
+```
+
+
